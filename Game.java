02c662c2fea2c43860b98edf732e93eb23efd9a4
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-27 09:45:38 -0700

    imported util scanner and created greeting and setup for guessing

--- Game.java
+++ Game.java
@@ -3,10 +3,15 @@
 A number-guessing game.
 
 */
+import java.util.Scanner;
 
 public class Game {
   public static void main(String[] args){
+    Scanner userInput = new Scanner(System.in);
     System.out.println("Hello, what is your name?");
+    String name = userInput.nextLine();
+    System.out.println(name + ", I'm thinking of a number between 1 and 100. Try to guess my number.");
+
   }
 
 }
